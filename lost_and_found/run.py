22112734eda44f51ab12ad5bea28e13eea7afdf2
ikksss <572179341@qq.com>
2019-06-05 12:21:15 +0800

    fix bug

--- lost_and_found/run.py
+++ lost_and_found/run.py
@@ -1,6 +1,10 @@
 from app import create_app
+from app import db
 
 app = create_app()
+app.app_context().push()
+db.create_all(app=app)
+
 
 if __name__ == "__main__":
     app.debug = app.config['DEBUG']
